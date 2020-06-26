b5471d15b95a83ddbbc58df2eec8d744c332daa5
Daeun Lee <ekdms7273@kookmin.ac.kr>
2020-06-26 13:43:42 +0900

    Issue #4 make calc directory and add wsgi 3 files

---
+++ calc/share.py
@@ -0,0 +1,14 @@
+html ="""
+<html>
+    <body>
+        <form method="get" action="">
+            a = <input type="number" name="a"><br>
+            b = <input type="number" name="b"><br><br>
+            <input type="submit">
+        </form>
+        sum =%(sum)s</br>
+        product =%(product)s</br>
+    </body>
+</html>
+"""
+
