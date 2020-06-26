56b37a865104976217642a933a8ad4ac9afc1427
Daeun Lee <ekdms7273@kookmin.ac.kr>
2020-06-26 16:32:55 +0900

    Issue #5 modified: sum,product=0 add: if a.isdigit() and b.isdigit()-> sum=a+b, product=a*b      else -> sum=-1, product=-1 =>사용자가 입력한 값이 없을 경우 -1을 출력하도록 함

--- calc/cal.py
+++ calc/cal.py
@@ -5,19 +5,21 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    sum = ''
-    product = ''
-    if '' not in [a, b]:
+    sum = 0 
+    product = 0 
+    if a.isdigit() and b.isdigit():        
         a, b = int(a), int(b)
         sum = a + b
         product = a * b
+    else:
+        sum = -1
+        product = -1 
     response_body = html %{
-        'sum': sum,
-        'product': product,
-    }
+        'sum': sum, 
+        'product': product,   
+    } 
     start_response('200 OK', [
-        ('Content-Type', 'text/html'),
-        ('Content-Length', str(len(response_body)))
+        ('Content-Type', 'text/html'), 
+        ('Content-Length', str(len(response_body))) 
     ])
-    return [response_body]
-
+    return [response_body] 
